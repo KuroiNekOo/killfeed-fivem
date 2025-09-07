af451dd51fc2b5f9dc3a6b0f49115e2ef885fd25
kuroiNekOo <maximegomes.dinis@outlook.fr>
2025-09-07 12:41:52 +0200

    remove killtest client command

--- client/commands.lua
+++ client/commands.lua
@@ -1,25 +1,25 @@
 print("^2[Killfeed] ^7Module Commands chargé")
 
 -- Fonction pour simuler un kill (pour les tests)
-function SimulatePlayerKill()
-    local playerPed = PlayerPedId()
-    local playerCoords = GetEntityCoords(playerPed)
+-- function SimulatePlayerKill()
+--     local playerPed = PlayerPedId()
+--     local playerCoords = GetEntityCoords(playerPed)
     
-    -- Simuler des données de kill
-    local isHeadshot = math.random(1, 3) == 1  -- 33% de chance
-    local distance = math.random(50, 400)      -- Distance aléatoire
+--     -- Simuler des données de kill
+--     local isHeadshot = math.random(1, 3) == 1  -- 33% de chance
+--     local distance = math.random(50, 400)      -- Distance aléatoire
 
-    -- Récupérer l'ID du joueur
-    local myServerId = GetPlayerServerId(PlayerId())
+--     -- Récupérer l'ID du joueur
+--     local myServerId = GetPlayerServerId(PlayerId())
 
-    -- Déclencher l'event serveur killerId, victimId, isHeadshot, distance
-    TriggerServerEvent('killfeed:playerKilled', myServerId, myServerId, isHeadshot, distance)
-end
+--     -- Déclencher l'event serveur killerId, victimId, isHeadshot, distance
+--     TriggerServerEvent('killfeed:playerKilled', myServerId, myServerId, isHeadshot, distance)
+-- end
 
 -- Commande de test côté client
-RegisterCommand('killtest', function()
-    SimulatePlayerKill()
-end, false)
+-- RegisterCommand('killtest', function()
+--     SimulatePlayerKill()
+-- end, false)
 
 -- Commande pour vérifier l'état de l'interface
 RegisterCommand('uicheck', function()
